0759af27e3095149d5c83e2676eec40021c89bac
Jarrod <me@jarrodrizor.com>
2015-09-13 18:51:52 -0400

    basic master blade file complete.

---
+++ resources/views/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leaderboard</title>
+    <link rel="stylesheet" href="{{ url("css/app.css") }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<div class="container">
+    @yield('scores')
+</div>
+
+</body>
+</html>

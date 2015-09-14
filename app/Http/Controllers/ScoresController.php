1424e143a06874d885093cf34da57c782dbb44d4
Jarrod <me@jarrodrizor.com>
2015-09-13 22:53:44 -0400

    return all data for index@ScoreController.

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -11,7 +11,7 @@ use App\Score;
 class ScoresController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listng of the resource.
      *
      * @return Response
      */

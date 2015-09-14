0111df98eefedb92c26b0b3e3644190fc297453d
Jarrod <me@jarrodrizor.com>
2015-09-13 22:53:13 -0400

    return all data for index@ScoreController.

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Score;
 
 class ScoresController extends Controller
 {
@@ -16,7 +17,8 @@ class ScoresController extends Controller
      */
     public function index()
     {
-        //
+        $score = Score::all();
+        return view('index')->with("score", $score);
     }
 
     /**

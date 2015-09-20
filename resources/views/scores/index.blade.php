447c7d30a6254b9b6fc3a38ac710fd1661f97e3d
Jarrod <me@jarrodrizor.com>
2015-09-20 19:49:23 -0400

    added twitter url

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -2,11 +2,14 @@
 
 @section("content")
 
-    <h2>Add new Score</h2>
+    <h2>Add New Score</h2>
     <form method="post" action="{{ url("scores") }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group">
             <input name="name" placeholder="full name" class="form-control">
+        </div>
+        <div class="form-group">
+            <input name="twitter" placeholder="Twitter" class="form-control">
         </div>
         <div class="form-group">
             <input name="score" placeholder="score" class="form-control">
@@ -15,6 +18,7 @@
             <input type="submit" value="Add User and Score" class="btn btn-primary form-control">
         </div>
     </form>
+    <hr>
 
     <!-- Check for errors -->
     @if($errors->any())
@@ -31,6 +35,7 @@
         <tr>
             <th>#</th>
             <th>Name</th>
+            <th>Twitter</th>
             <th>Score</th>
             <th>Delete</th>
         </tr>
@@ -38,6 +43,12 @@
             <tr>
                 <td>{{ ++$index }}</td>
                 <td>{{ $score->name }}</td>
+                <td><a href="http://twitter.com/{{ $score->twitter }}" target="_blank">
+                        <!-- Check if user has a twitter -->
+                        @if($score->twitter)
+                            {{"@" . $score->twitter }}</a>
+                        @endif
+                </td>
                 <td>{{ $score->score }}</td>
                 <td><button class="btn-danger">Delete</button></td>
             </tr>

3a0d6bb0b02fd45ea125b12a08f75f0d9845958e
Jarrod <me@jarrodrizor.com>
2015-09-20 17:43:17 -0400

    minor template changes.

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -2,6 +2,7 @@
 
 @section("content")
 
+    <h2>Add new Score</h2>
     <form method="post" action="{{ url("scores") }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group">
@@ -11,11 +12,21 @@
             <input name="score" placeholder="score" class="form-control">
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary">
+            <input type="submit" value="Add User and Score" class="btn btn-primary form-control">
         </div>
     </form>
 
+    <!-- Check for errors -->
+    @if($errors->any())
+        <ul class="alert alert-danger">
+        @foreach($errors->all() as $error)
+              <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    @endif
 
+    <h2>Leaderboard</h2>
+    <!-- Loop Scores -->
     @foreach($scores as $score)
         <p>{{ $score->name }}
             <span>Scores: {{ $score->score }}</span>

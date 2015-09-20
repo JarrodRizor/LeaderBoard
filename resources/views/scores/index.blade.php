780e9a9230ad4b277ec143c8a1699737875cfe53
Jarrod <me@jarrodrizor.com>
2015-09-20 14:58:09 -0400

    added twitter BS and a basic form.

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -1,6 +1,17 @@
 @extends("master")
 
 @section("content")
+
+    <form method="post" action="{{ url("scores") }}">
+        <div class="form-group">
+            <input name="name" placeholder="full name" class="form-control">
+        </div>
+        <div class="form-group">
+            <input name="score" placeholder="score" class="form-control">
+        </div>
+    </form>
+
+
     @foreach($scores as $score)
         <p>{{ $score->name }}
             <span>Scores: {{ $score->score }}</span>
